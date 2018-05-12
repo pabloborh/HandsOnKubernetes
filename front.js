f183188bb4ec32c87abeb8a70d7ed3a0a00842d9
pabborh1 <pabborh1@inf.upv.es>
2018-05-12 19:17:39 +0200

    Front app v1.0

---
+++ front.js
@@ -0,0 +1,37 @@
+//cliente REQ
+var zmq = require('zeromq');
+
+var front = zmq.socket('req')
+
+var front_ID = Math.trunc(Math.random() * (10000 - 1) + 1)
+
+front.on('message',function (respuesta){
+	console.log("respuesta recibida ["+respuesta.toString()+"]");
+	
+});
+
+front.connect("tcp://127.0.0.1:9000");
+
+var jsonMessage='{"Topic": "KubernetesIsAwesome", "Message": "Hola ke ase"}'
+
+console.log("Enviando.. "+jsonMessage)
+
+front.send(jsonMessage);
+
+// WEBSITE //
+
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello from Kubernetes!! Soy ' + front_ID + '\n');
+}).listen(8080, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:8080/');
+
+////////////
+
+process.on('SIGINT',function(){
+	console.log("sigint capturada ! ");
+	socketRep.close();
+	socketRep = null;
+
+});
